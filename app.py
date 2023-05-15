70ca24e9e9bb1e258809cb55f0429b376bf5c180
kolamanoj <kollamanoj19@gmail.com>
2023-05-15 15:30:07 +0530

    upadte

---
+++ app.py
@@ -0,0 +1,8 @@
+a='radar'
+b='manoj'
+
+ print(type(a),type(b))
+  #check - palindrome
+print(a==a[::-1]) # 'radar' == 'radar' -true
+print(b==b[::-1]) # 'manoj' == 'uhdam'-false
+print('hello manoj')
